2c5339c61e4e8044db59481dd633969d5fb24cc8
Md Raz <75399430+md-raz@users.noreply.github.com>
2023-12-01 13:20:46 -0500

    Update TROJAN_TOP.vhd

--- OffRAMPS_HDL.srcs/sources_1/new/TROJAN_TOP.vhd
+++ OffRAMPS_HDL.srcs/sources_1/new/TROJAN_TOP.vhd
@@ -3,11 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity TROJAN_TOP is
     Port (
-        i_CLK                 : in  std_logic;
-        i_RESET                : in std_logic;
-        enable_x_troj       : in  std_logic;
-        enable_y_troj       : in  std_logic;
-        enable_z_troj       : in  std_logic;
+        i_CLK               : in  std_logic;
         homing_complete     : in  std_logic;
         
         -- Data Signals In
@@ -53,62 +49,82 @@ entity TROJAN_TOP is
 end TROJAN_TOP;
 
 architecture Behavioral of Trojan_TOP is
-    
+
+    COMPONENT PULSE_GEN
+    PORT ( 
+        i_CLK           : in  STD_LOGIC;
+        i_PULSE_EN      : in  STD_LOGIC;
+        i_PULSES_TO_SEND: in  std_logic_vector(4 downto 0);
+        o_PULSE_SIG     : out STD_LOGIC;
+        o_COMPLETE      : out STD_LOGIC
+    );
+    END COMPONENT;
+
+    COMPONENT RISING_EDGE_DETECTOR
+	PORT(
+        i_CLK     : in  STD_LOGIC;
+        input     : in  STD_LOGIC;
+        output    : out STD_LOGIC
+		);
+	END COMPONENT;
+	   
     -- Edge Detected signals
     signal s_edge_x_step : std_logic := '0';
     signal s_edge_y_step : std_logic := '0';
     signal s_edge_z_step : std_logic := '0';
     signal s_edge_e_step : std_logic := '0';
 
-
     -- Pulse Related Signals per Axis
-    constant X_PULSES_PER_STEP : std_logic_vector(5 downto 0) := X"10";  -- 16 pulses per step --> 1.8 degrees (?)
-    signal X_PULSE_COUNT : std_logic_vector (5 downto 0) := (others=>'0');
-    signal X_PULSE_COUNT_EN : std_logic := '0';
+    constant PULSES_PER_STEP : std_logic_vector(5 downto 0) := "10000";  -- 16 pulses per step --> 1.8 degrees (?)
+    
     signal X_STEP_MOD : std_logic := '0';
-
-    constant Y_PULSES_PER_STEP : std_logic_vector(5 downto 0) := X"10";  -- 16 pulses per step --> 1.8 degrees (?)
-    signal Y_PULSE_COUNT : std_logic_vector (5 downto 0) := (others=>'0');
-    signal Y_PULSE_COUNT_EN : std_logic := '0';
     signal Y_STEP_MOD : std_logic := '0';
-
-    constant Z_PULSES_PER_STEP : std_logic_vector(5 downto 0) := X"10";  -- 16 pulses per step --> 1.8 degrees (?)
-    signal Z_PULSE_COUNT : std_logic_vector (5 downto 0) := (others=>'0');
-    signal Z_PULSE_COUNT_EN : std_logic := '0';
     signal Z_STEP_MOD : std_logic := '0';
-
-    constant E_PULSES_PER_STEP : std_logic_vector(5 downto 0) := X"10";  -- 16 pulses per step --> 1.8 degrees (?)
-    signal E_PULSE_COUNT : std_logic_vector (5 downto 0) := (others=>'0');
-    signal E_PULSE_COUNT_EN : std_logic := '0';
     signal E_STEP_MOD : std_logic := '0';
 
+    signal X_PULSE_EN : std_logic := '0';
+    signal Y_PULSE_EN : std_logic := '0';
+    signal Z_PULSE_EN : std_logic := '0';
+    signal E_PULSE_EN : std_logic := '0';
 
-    -- We will determine the time between pulses here, no acceleration config (yet)
--- We will send the pulses @ 6.5kHz. That is (at 100 Mhz):
+    signal X_STEP_COMPLETE : std_logic := '0';
+    signal Y_STEP_COMPLETE : std_logic := '0';
+    signal Z_STEP_COMPLETE : std_logic := '0';
+    signal E_STEP_COMPLETE : std_logic := '0';
 
-
-    COMPONENT RISING_EDGE_DETECTOR
-	PORT(
-        i_CLK     : in  std_logic;
-        input     : in  std_logic;
-        output    : out std_logic
-		);
-	END COMPONENT;
+    -- Temporarily we will set the enabled trojans here, hardcoded. Vivado will optimize out the unused ones.
+    signal TROJ_T1_ENABLE : std_logic := '0'; -- Randomly adds or removes steps from X, Y, or Z axis
+        
+    signal TROJ_T2_ENABLE : std_logic := '0'; -- Constant over / under extrusion per print
+    signal TROJ_T3_ENABLE : std_logic := '0'; -- Increases or decreases filament retraction between layers
+    signal TROJ_T4_ENABLE : std_logic := '0'; -- Small Shift along X and Y axis on random Z layer increment
+    signal TROJ_T5_ENABLE : std_logic := '0'; -- Denial of service via disabling D8/D10 heating element power
+    signal TROJ_T6_ENABLE : std_logic := '0'; -- Spoofing of measured hot-end thermocouple temperatures via ADC
+    signal TROJ_T7_ENABLE : std_logic := '0'; -- 
+    signal TROJ_T8_ENABLE : std_logic := '0'; -- 
     
 begin
 
     ------- Components--------- 
-    
+    -- Edge Detectors 
     X_STEP_EDGE : RISING_EDGE_DETECTOR PORT MAP(i_CLK => i_CLK, input => i_X_STEP, output => s_edge_x_step);
     Y_STEP_EDGE : RISING_EDGE_DETECTOR PORT MAP(i_CLK => i_CLK, input => i_Y_STEP, output => s_edge_y_step);
     Z_STEP_EDGE : RISING_EDGE_DETECTOR PORT MAP(i_CLK => i_CLK, input => i_Z_STEP, output => s_edge_z_step);
     E_STEP_EDGE : RISING_EDGE_DETECTOR PORT MAP(i_CLK => i_CLK, input => i_E0_STEP, output => s_edge_e_step);
+    
+    -- Pulse Generators
+    X_PULSE_GEN : PULSE_GEN PORT MAP (i_CLK => i_CLK, i_PULSE_EN => X_PULSE_EN, i_PULSES_TO_SEND => PULSES_PER_STEP, o_PULSE_SIG => X_STEP_MOD, o_COMPLETE => X_STEP_COMPLETE);
+    Y_PULSE_GEN : PULSE_GEN PORT MAP (i_CLK => i_CLK, i_PULSE_EN => Y_PULSE_EN, i_PULSES_TO_SEND => PULSES_PER_STEP, o_PULSE_SIG => Y_STEP_MOD, o_COMPLETE => Y_STEP_COMPLETE);
+    Z_PULSE_GEN : PULSE_GEN PORT MAP (i_CLK => i_CLK, i_PULSE_EN => Z_PULSE_EN, i_PULSES_TO_SEND => PULSES_PER_STEP, o_PULSE_SIG => Z_STEP_MOD, o_COMPLETE => Z_STEP_COMPLETE);
+    E_PULSE_GEN : PULSE_GEN PORT MAP (i_CLK => i_CLK, i_PULSE_EN => E_PULSE_EN, i_PULSES_TO_SEND => PULSES_PER_STEP, o_PULSE_SIG => E_STEP_MOD, o_COMPLETE => E_STEP_COMPLETE);
+    
     ------- Logic -------------
-    x_trojan_proc : process (i_CLK)
+    TROJAN_T1_proc : process (i_CLK)
     begin
         if rising_edge(i_CLK) then
-        
-
+            if (TROJ_T1_ENABLE = '1') then
+            
+            end if;
         end if;
     end process;
 
